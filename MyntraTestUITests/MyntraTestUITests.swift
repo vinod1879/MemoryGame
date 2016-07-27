ec91e987ddca7b8a0ed5d91cc73a12c0247ff56a
Vinod Vishwanath <vinod.v@redbus.in>
2016-07-28 03:51:22 +0530

    Wrote sample UI-Unit-Test-Case

--- MyntraTestUITests/MyntraTestUITests.swift
+++ MyntraTestUITests/MyntraTestUITests.swift
@@ -29,8 +29,66 @@ class MyntraTestUITests: XCTestCase {
     }
     
     func testExample() {
-        // Use recording to get started writing UI tests.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        
+        let completionExpectation = expectationWithDescription("Random Test Method")
+        
+        let delayTime = dispatch_time(DISPATCH_TIME_NOW, Int64(20.0 * Double(NSEC_PER_SEC)))
+        dispatch_after(delayTime, dispatch_get_main_queue()) {
+            
+            XCUIDevice.sharedDevice().orientation = .Portrait
+            
+            let collectionViewsQuery = XCUIApplication().collectionViews
+            let image = collectionViewsQuery.childrenMatchingType(.Cell).elementBoundByIndex(0).otherElements.childrenMatchingType(.Image).element
+            image.tap()
+            
+            let image2 = collectionViewsQuery.childrenMatchingType(.Cell).elementBoundByIndex(1).otherElements.childrenMatchingType(.Image).element
+            image2.tap()
+            
+            let image3 = collectionViewsQuery.childrenMatchingType(.Cell).elementBoundByIndex(2).otherElements.childrenMatchingType(.Image).element
+            image3.tap()
+            
+            let image4 = collectionViewsQuery.childrenMatchingType(.Cell).elementBoundByIndex(5).otherElements.childrenMatchingType(.Image).element
+            image4.tap()
+            
+            let image5 = collectionViewsQuery.childrenMatchingType(.Cell).elementBoundByIndex(4).otherElements.childrenMatchingType(.Image).element
+            image5.tap()
+            
+            let image6 = collectionViewsQuery.childrenMatchingType(.Cell).elementBoundByIndex(3).otherElements.childrenMatchingType(.Image).element
+            image6.tap()
+            
+            let image7 = collectionViewsQuery.childrenMatchingType(.Cell).elementBoundByIndex(6).otherElements.childrenMatchingType(.Image).element
+            image7.tap()
+            
+            let image8 = collectionViewsQuery.childrenMatchingType(.Cell).elementBoundByIndex(8).otherElements.childrenMatchingType(.Image).element
+            image8.tap()
+            
+            let image9 = collectionViewsQuery.childrenMatchingType(.Cell).elementBoundByIndex(9).otherElements.childrenMatchingType(.Image).element
+            image9.tap()
+            image.tap()
+            image2.tap()
+            image3.tap()
+            image.tap()
+            image2.tap()
+            image5.tap()
+            image4.tap()
+            image7.tap()
+            image9.tap()
+            image8.tap()
+            image6.tap()
+            image.tap()
+            image2.tap()
+            image7.tap()
+            image9.tap()
+            image5.tap()
+            image.tap()
+            image7.tap()
+            image9.tap()
+            
+            
+            completionExpectation.fulfill()
+        }
+        
+        waitForExpectationsWithTimeout(120, handler: nil)
     }
     
 }
